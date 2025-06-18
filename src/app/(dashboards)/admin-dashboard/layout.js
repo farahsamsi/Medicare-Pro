28bb39c9767b6bd488e431fac51d5dab720d1860
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-06-18 18:45:15 +0600

    made the admin dashboard specifice for admin from getting role from local storage

--- src/app/(dashboards)/admin-dashboard/layout.js
+++ src/app/(dashboards)/admin-dashboard/layout.js
@@ -1,8 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
 import AdminSidebarLayout from "./components/AdminSidebarLayout";
 import SetCalender from "./components/SetCalender";
 import SetClock from "./components/SetClock";
+import { useRouter } from "next/navigation";
 
 export default function AdminDashboardLayout({ children }) {
+  const router = useRouter();
+  const [isAdmin, setIsAdmin] = useState(null);
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+
+    if (userData) {
+      const parsed = JSON.parse(userData);
+      if (parsed?.role === "super_admin") {
+        setIsAdmin(true);
+      } else {
+        router.replace("/");
+      }
+    } else {
+      router.replace("/");
+    }
+  }, [router]);
+
+  if (!isAdmin) return null;
+
   return (
     <AdminSidebarLayout>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-1">
